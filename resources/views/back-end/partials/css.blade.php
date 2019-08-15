430e1cce9e1ee46625371fb8a5ed10a5246b6641
NgoUyUong <ngouyoung@yahoo.com>
2019-08-15 17:40:09 +0700

    Initial template

---
+++ resources/views/back-end/partials/css.blade.php
@@ -0,0 +1,22 @@
+<!-- Bootstrap 3.3.7 -->
+<link rel="stylesheet" href="{{ asset('node_modules/bootstrap/dist/css/bootstrap.min.css') }}">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{ asset('node_modules/font-awesome/css/font-awesome.min.css') }}">
+<!-- Ionicons -->
+<link rel="stylesheet" href="{{ asset('node_modules/ionicons/dist/css/ionicons.min.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('css/AdminLTE.min.css') }}">
+<!-- AdminLTE Skins. Choose a skin from the css/skins
+     folder instead of downloading all of them to reduce the load. -->
+<link rel="stylesheet" href="{{ asset('css/skins/_all-skins.min.css') }}">
+<!-- Morris chart -->
+<link rel="stylesheet" href="{{ asset('node_modules/morris.js/morris.css') }}">
+<!-- jvectormap -->
+<link rel="stylesheet" href="{{ asset('node_modules/jvectormap/jquery-jvectormap.css') }}">
+<!-- Date Picker -->
+<link rel="stylesheet"
+      href="{{ asset('node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
+<!-- Daterange picker -->
+<link rel="stylesheet" href="{{ asset('node_modules/bootstrap-daterangepicker/daterangepicker.css') }}">
+<!-- bootstrap wysihtml5 - text editor -->
+<link rel="stylesheet" href="{{ asset('plugin/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
---
+++ resources/views/back-end/partials/script.blade.php
@@ -0,0 +1,37 @@
+<!-- jQuery 3 -->
+<script src="{{ asset('node_modules/jquery/dist/jquery.min.js') }}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{ asset('plugin/jquery-ui/jquery-ui.min.js') }}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{ asset('node_modules/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- Morris.js charts -->
+<script src="{{ asset('node_modules/raphael/raphael.min.js') }}"></script>
+<script src="{{ asset('node_modules/morris.js/morris.min.js') }}"></script>
+<!-- Sparkline -->
+<script src="{{ asset('node_modules/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
+<!-- jvectormap -->
+<script src="{{ asset('node_modules/jvectormap-next/jquery-jvectormap.min.js') }}"></script>
+<script src="{{ asset('plugin/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{ asset('node_modules/jquery-knob/dist/jquery.knob.min.js') }}"></script>
+<!-- daterangepicker -->
+<script src="{{ asset('node_modules/moment/min/moment.min.js') }}"></script>
+<script src="{{ asset('node_modules/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+<!-- datepicker -->
+<script src="{{ asset('node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="{{ asset('plugin/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
+<!-- Slimscroll -->
+<script src="{{ asset('node_modules/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{ asset('node_modules/fastclick/lib/fastclick.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('js/adminlte.min.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset('js/pages/dashboard.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('js/demo.js') }}"></script>
